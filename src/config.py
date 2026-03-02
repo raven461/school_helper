baf2f45144206981133d14e4737dc24ec37b1968
cibergoffushakovgs-ux <cibergoff.ushakovgs@gmail.com>
2026-03-02 23:37:51 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -10,11 +10,12 @@ class Settings(BaseSettings):
     bot_token: SecretStr
     ai_token: SecretStr
     dev_key: SecretStr
-    
+    email_key: SecretStr
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), "../.env"),
         env_file_encoding='utf-8',
         extra='ignore'
     )
+
 
 config = Settings()
