2847ba85ee865b382ff06982a83be0b52ab0c1a4
raven461 <ravenvan@mail.ru>
2026-03-02 22:02:28 +0300

    Optimized big lists in "bd_worker"

--- src/system/bd_worker.py
+++ src/system/bd_worker.py
@@ -2,9 +2,6 @@ from os import path
 import sqlite3
 import json
 import time
-if not path.exists("../user_information.db"):
-    with open("../user_information.db","w"):#костыльное создание файла
-        pass
 conn = sqlite3.connect('users_information.db', check_same_thread=False)
 
 def init_db():
