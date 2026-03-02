2c0b4d8f88d2f3e4249a26dd97bccb9784e6e747
cibergoffushakovgs-ux <cibergoff.ushakovgs@gmail.com>
2026-03-03 00:19:22 +0300

    Added email logic

--- src/system/email.py
+++ src/system/email.py
@@ -1,1 +1,16 @@
-
+from config import config, email_login
+import smtplib
+class Email:
+  def __init__(self, login=email_login):
+    self.login = login
+    self.smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    self.smtpObj.starttls()
+    password =config.email.get_secret_value()
+    self.smtpObj.login(login,password)
+    
+  def send_email(self, receiver, text):
+    self.smtpObj.sendmail(self.login, receiver, text)
+  def exit(self):
+    self.smtpObj.quit()
+    
+    
