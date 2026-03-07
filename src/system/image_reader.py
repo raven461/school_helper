d9fb75a59a529f486fe705c018c14e91d2216928
raven461 <ravenvan@mail.ru>
2026-03-07 19:18:59 +0300

    Created right /src/system/image_reader

--- src/system/image_reader.py
+++ src/system/image_reader.py
@@ -1,8 +1,15 @@
+from aiogram import Bot
 from aiogram.types import PhotoSize
 from PIL import Image
 import pytesseract
+import logging
 
-#TODO:доделать получение фотографий
-async def text_from_tg_photo(photo: PhotoSize, lang: str = "rus+eng") -> str:
-    image = Image.open(photo)
+async def text_from_tg_photo(bot: Bot,photo: PhotoSize, lang: str = "rus+eng") -> str:
+    file = await bot.get_file(photo.file_id)
+    file_path = file.file_path
+    if file_path is not None:
+        logging.error(f"GetPhotoError: photo with id: {photo.file_id} has None path")
+        return ""
+    downloaded_file = await bot.download_file(file_path)
+    image = Image.open(downloaded_file)
     return pytesseract.image_to_string(image, lang=lang)
