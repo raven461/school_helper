7b07d34feea8c556b5536b946fc98d0a9633252b
Иван <ravenvan@mail.ru>
2026-03-11 22:29:48 +0300

    Closed issue #36

--- src/bot.py
+++ src/bot.py
@@ -9,7 +9,6 @@ async def main():
     await init_db()
     bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher()
-    asyncio.create_task(schedule_handler.update_data_task(),name = "updating_schedule_task")
     dp.include_routers(common.router,
                        ai_handler.router,
                        schedule_handler.router,
@@ -17,14 +16,22 @@ async def main():
                        achievments_handler.router,
                        dev_handler.router)
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    background_task = asyncio.create_task(schedule_handler.update_data_task())
+    try:
+        await dp.start_polling(bot)
+    finally:
+        background_task.cancel()
+        try:
+            await background_task
+        except asyncio.CancelledError:
+            pass
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         filename=LOG_FILE_PATH,
         encoding = LOG_FILE_ENCODING
     )
+    logging.info("Bot started")
     logging.getLogger("aiogram").setLevel(logging.INFO)
     asyncio.run(main())
-    logging.info("Bot runned")
