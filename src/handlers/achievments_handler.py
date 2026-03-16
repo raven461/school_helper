a1e73683b6a7c41ba34e591ab6983217168b9f86
Иван <ravenvan@mail.ru>
2026-03-16 20:38:48 +0300

    Changed achievements_handler (#44)

--- src/handlers/achievments_handler.py
+++ src/handlers/achievments_handler.py
@@ -1,23 +1,29 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command
-from system.database.enties import UserProgressController
-from achievements.achievements import Achievements
+from system.database.enties import UserProgressController,AchievementsController
 import logging
 
 router = Router()
-
-async def update_achivments():
-    achievements = Achievements()
          
 @router.message(Command("achievements"))
 async def get_achievements(message: Message):
     if message.from_user is None:
         logging.error("UserError: user params is empty")
         return
-    user_progress = await UserProgressController().create(message.from_user.id)
+    user_progress = await UserProgressController.create(message.from_user.id)
+    report = ""
     try:
-        await message.answer("\n".join(user_progress.get_user_achievements()))
+        ach_controller = AchievementsController()
+        for i in await ach_controller.get_all_ach():
+            if i.id in user_progress.achievements:
+                report += "<i>Название:</i> " + i.name + "\n"
+                report += "<i>Описание:</i> " + i.desc + "\n"
+                report += "\n"
+        if report != "":
+            await message.answer(report, parse_mode = "HTML")
+        else:
+            await message.answer("Пока нет ни одного достижения, но это скоро изменится! :)")
     except:
         await message.answer("Пока нет ни одного достижения, но это скоро изменится! :)")
     finally:
@@ -28,7 +34,7 @@ async def profile(message: Message):
     if message.from_user is None:
         logging.error("UserError: user params is empty")
         return
-    user_progress = await UserProgressController().create(message.from_user.id)
+    user_progress = await UserProgressController.create(message.from_user.id)
     await message.answer("Name: " + message.from_user.first_name)
     await message.answer("Exp: " + str(user_progress.exp))
     await message.answer("Готово")
