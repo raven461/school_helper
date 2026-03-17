feb296f513a5f47b8f0153390c1817a7755b2678
Иван <ravenvan@mail.ru>
2026-03-17 16:18:40 +0300

    Deleted conf directory

--- conf/exp/raito.py
+++
@@ -1,4 +0,0 @@
-class TaskRaitos:
-    BASE_EXP_RAITO = 0.7
-    FAILED_EXP_RAITO = 0.1
-ACHIEVMENT_EXP_RAITO = 0.4
--- conf/logging/file_opening.py
+++
@@ -1,3 +0,0 @@
-import datetime
-LOG_FILE_ENCODING = "utf-8"
-LOG_FILE_PATH = "./logs/log "+(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))+".log"
